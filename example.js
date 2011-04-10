54a2a1d3dba67e58985e32c4b35e03c687508f16
Your Name <cyruzdraxs@gmail.com>
2011-04-10 12:51:06 -0700

    Added prev() and args can be passed to callbacks

--- example.js
+++ example.js
@@ -1,23 +1,30 @@
 var chainer = require('./index');
 var chain = new chainer();
 var count = 0;
+var max_calls = 20;
 
-// Add three functions to chain.
+// Add four functions to chain.
+// These are designed to ping-pong
+// back and forth between first and fourth.
 chain.add(function(){
 	console.log('first');
-	counter(20, chain.next, true);
+	counter(true, chain.next);
 });
 chain.add(function(dir){
 	console.log('second');
-	counter(20, dir ? chain.next : chain.prev, dir);
+	counter(dir, dir ? chain.next : chain.prev);
 });
 chain.add(function(dir){
 	console.log('third');
-	counter(20, chain.prev, !dir);
+	counter(dir, dir ? chain.next : chain.prev);
+});
+chain.add(function(){
+	console.log('fourth');
+	counter(false, chain.prev);
 });
 
 // Execute chain from end.
-chain.from('end', true);
+chain.from('end');
 
 /**
  * Other options include;
@@ -26,14 +33,15 @@ chain.from('end', true);
  * 	chain.run();
  */
 
-// Only executes the callback
-// if the counter limit hasn't been reached.
-function counter(max_calls, callback) {
+// Only executes the callback if the
+// counter limit hasn't been reached.
+function counter() {
 	count++;
 	args = [];
-	for (var i = 2; i < arguments.length; i++) {
+	for (var i = 0; i < (arguments.length-1); i++) {
 		args.push(arguments[i]);
 	}
+	var callback = arguments[arguments.length-1];
 	if (count < max_calls) {
 		callback.apply(null, args);
 	}
